07f8ebf706257e460439a80dc8be303190e073b1
Rafi Mahafid <rafimahafid@gmail.com>
2020-10-19 21:12:47 +0600

    strongAtk

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
-let ATTACK_POWER = 10;
+const ATTACK_POWER = 10;
+const STRONG_ATTACK_POWER = 15;
+const MONSTER_POWER = 13;
 
 let chooseMaxLife = 100;
 
@@ -7,12 +9,33 @@ let playerLife = chooseMaxLife;
 
 adjustHealthBars(chooseMaxLife);
 
-function attackFunction() {
-    let damage = dealMonsterDamage(ATTACK_POWER);
+function attackMonster(mode) {
+    if (mode === 'ATTACK') {
+        mode = ATTACK_POWER;
+    } else if (mode === 'STRONG_ATTACK') {
+        mode = STRONG_ATTACK_POWER;
+    }
+
+    let damage = dealMonsterDamage(mode);
     monsterLife -= damage;
-    if (monsterLife <= 0) {
+    let playerDamage = dealPlayerDamage(MONSTER_POWER);
+    playerLife -= playerDamage;
+    if (monsterLife <= 0 && playerLife > 0) {
         alert('You win!');
+    } else if (playerLife <= 0 && monsterLife > 0) {
+        alert('You lose!');
+    } else if (playerLife <= 0 && monsterLife <= 0) {
+        alert('Draw');
     }
 }
 
-attackBtn.addEventListener('click', attackFunction);
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
